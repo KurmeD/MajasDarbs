2ebd8163ef02b151d91660fef3965c78e57851b6
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-04-26 15:55:46 +0300

    db3_12

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLALchemy
 
 app = Flask('app')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 class test(db.Model):
@@ -19,28 +18,27 @@ class test(db.Model):
 
 @app.route('/')
 def root():
-  return ("sveika pasule")
-#  return render_template('index.html')
+  return render_template('index.html')
 #  return render_template('atminasspele.html')
 
-#@app.route('/atminasspele')
-#def spele():
- # return render_template('atminasspele.html')
+@app.route('/atminasspele')
+def spele():
+ return render_template('atminasspele.html')
 
-#@app.route('/kalkulators')
-#def kalkulators():
-#  return render_template('kalkulators.html')
+@app.route('/kalkulators')
+def kalkulators():
+  return render_template('kalkulators.html')
   
-#@app.route('/saites')
-#def saites():
-#  return render_template('saites.html')
-#@app.route('/veidotaji')
-#def veidotaji():
-#  return render_template('veidotaji.html')
+@app.route('/saites')
+def saites():
+  return render_template('saites.html')
+@app.route('/veidotaji')
+def veidotaji():
+  return render_template('veidotaji.html')
 
-#@app.route('/rezultati')
-#def results():
-#  return render_template('rezultati.html')
+@app.route('/rezultati')
+def results():
+  return render_template('rezultati.html')
 
 @app.route('/postgreSQL')
 def postgreSQL():
