ea64139dfe2631dbafa8719ccb4f5aa7ad8d705f
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-04-26 17:02:25 +0300

    db5_5

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask('app')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+
 class test(db.Model):
   col =db.Column(db.String(255), primary_key=True)
   col2 = db.Column(db.String(255), unique=True, nullable=False) 
