c6eec4dfcd5286a15f1dc406428970b04a4680d4
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-04-26 15:52:48 +0300

    db3_11

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 
 class test(db.Model):
   col =db.Column(db.String(255), primary_key=True)
-  col2 = c) 
+  col2 = db.Column(db.String(255), unique=True, nullable=False) 
     
     def __repr__ (self):
       return '%r' % self.col
