4d5b7e82289cc3ebf727349dbfc534741d966655
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-04-27 21:05:54 +0300

    chats1

--- main.py
+++ main.py
@@ -2,20 +2,20 @@
 
 #app = Flask(__name__)
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 from flask_sqlalchemy import SQLALchemy
 
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#db = SQLAlchemy(app)
 
 
-class test(db.Model):
-  col =db.Column(db.String(255), primary_key=True)
-  col2 = db.Column(db.String(255), unique=True, nullable=False) 
+#class test(db.Model):
+ # col =db.Column(db.String(255), primary_key=True)
+  #col2 = db.Column(db.String(255), unique=True, nullable=False) 
     
-    def __repr__ (self):
-      return '%r' % self.col
+   # def __repr__ (self):
+   #   return '%r' % self.col
 
 @app.route('/')
 def root():
@@ -41,12 +41,41 @@ def veidotaji():
 def results():
   return render_template('rezultati.html')
 
-@app.route('/postgreSQL')
-def postgreSQL():
-  result=test.query.all()
-  return '%r' % result
+@app.route('/chats')
+def chat():
+  return render_template('chats.html')
+
+@app.route('/health')
+def health_check():
+  return "OK"
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas=[]
+  with open("chats.txt", "r", encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+      
+  return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats/suuti', methods= ['POST'])
+def suuti_zinju():
+  dati = request.json
+
+  with open("chats.txt", "a", newline="", encoding="UTF-8") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
+
+
+
+#@app.route('/postgreSQL')
+#def postgreSQL():
+#  result=test.query.all()
+#  return '%r' % result
 
 #app.run(host='0.0.0.0', port=8020)
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support 5000
-    app.run(threaded=True, port=5432)
+    app.run(threaded=True, port=5000, debug=True)
