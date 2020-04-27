c398ef4c4d34148fe5850518887ce099a934e588
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-04-27 22:48:20 +0300

    db123

--- main.py
+++ main.py
@@ -3,19 +3,18 @@
 #app = Flask(__name__)
 import os
 from flask import Flask, render_template, json, jsonify, request
-from flask_sqlalchemy import SQLALchemy
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask('app')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-
 class test(db.Model):
-  col =db.Column(db.String(255), primary_key=True)
-  col2 = db.Column(db.String(255), unique=True, nullable=False) 
-    
-    def __repr__ (self):
-      return '%r' % self.col
+    col = db.Column(db.String(255), primary_key=True)
+    col2 = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '%r' % self.col
 
 @app.route('/')
 def root():
@@ -71,8 +70,8 @@ def suuti_zinju():
 
 
 @app.route('/postgreSQL')
-def postgreSQL():
-  result=test.query.all()
+def postgresSQL():
+  result = test.query.all()
   return '%r' % result
 
 #app.run(host='0.0.0.0', port=8020)
